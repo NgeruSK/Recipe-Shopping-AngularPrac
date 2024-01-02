ba78805b212c921c0a78ff9ca30bf2a57e306993
ngeru.sk <ngeru.sk@gmail.com>
2024-01-02 17:54:02 +0300

    skeleton of app driven

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+  recipes:Recipe[] = [
+    new Recipe('mango veggies','has mangoes','https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?b=1&s=612x612&w=0&k=20&c=V8oaDpP3mx6rUpRfrt2L9mZCD0_ySlnI7cd4nkgGAb8='),
+    new Recipe('Test Recipe','has oranges','https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D'),
+
+  ];
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+}
+
